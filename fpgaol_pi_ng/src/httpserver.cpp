4aa43e157bf3eb8c0ed60595a932293cae01e0ed
CHEN Yihui <chenyihui@cyh.ac.cn>
2020-04-19 22:48:27 +0800

    add Access-Control-Allow-Origin

--- fpgaol_pi_ng/src/httpserver.cpp
+++ fpgaol_pi_ng/src/httpserver.cpp
@@ -22,42 +22,51 @@ void handler::service(stefanfrings::HttpRequest& request, stefanfrings::HttpResp
     qDebug("RequestMapper: path=%s",path.data());
     if (path.startsWith("/upload"))
     {
-        QTemporaryFile* file=request.getUploadedFile("bitstream");
-        QDateTime now = QDateTime::currentDateTime();
-        QString file_name = "../../" + now.toString("yyyyMMddHHmmss") + ".bit";
-        QFile file_save(file_name);
-        if (!file_save.open((QIODevice::WriteOnly))) {
-            qDebug("save bitstream file failed");
-            return;
-        }
-        if (file)
-        {
-            while (!file->atEnd() && !file->error())
-            {
-                QByteArray buffer=file->read(65536);
-                file_save.write(buffer);
+        QByteArray method = request.getMethod();
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+        if (method == "POST") {
+            QTemporaryFile* file=request.getUploadedFile("bitstream");
+            QDateTime now = QDateTime::currentDateTime();
+            QString file_name = "../../" + now.toString("yyyyMMddHHmmss") + ".bit";
+            QFile file_save(file_name);
+            if (!file_save.open((QIODevice::WriteOnly))) {
+                qDebug("save bitstream file failed");
+                return;
             }
-            if (file->atEnd()) {
-                file_save.close();
-                int r = programFPGA(file_name);
-                if (r) {
-                    qDebug() << "program failed, ret" << r;
-                    response.setStatus(500);
-                    response.write("Program failed", true);
+            if (file)
+            {
+                while (!file->atEnd() && !file->error())
+                {
+                    QByteArray buffer=file->read(65536);
+                    file_save.write(buffer);
                 }
-            } else {
-                qDebug("upload file error");
+                if (file->atEnd()) {
+                    file_save.close();
+                    int r = programFPGA(file_name);
+                    if (r) {
+                        qDebug() << "program failed, ret" << r;
+                        response.setStatus(500);
+                        response.write("Program failed", true);
+                    }
+                } else {
+                    qDebug("upload file error");
+                    response.setStatus(500);
+                    response.write("upload failed", true);
+                }
+                // TODO(programming code)
+                response.setStatus(200);
+                response.write("Program success!", true);
+            }
+            else
+            {
                 response.setStatus(500);
                 response.write("upload failed", true);
             }
-            // TODO(programming code)
-            response.setStatus(200);
-            response.write("Program success!", true);
-        }
-        else
-        {
-            response.setStatus(500);
-            response.write("upload failed", true);
+        } else if (method == "OPTIONS") {
+            response.setStatus(204);
+            response.write("", true);
         }
     }
     else
