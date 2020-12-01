a7a77559cdb4717f58a06568afb74bbb22654c00
thisiszy <hzy_ustc@mail.ustc.edu.cn>
2020-12-01 23:37:19 +0800

    remove restart botton

--- docroot/static/js/room.js
+++ docroot/static/js/room.js
@@ -115,18 +115,6 @@ $(document).ready(function () {
     var filestatus = $("#filestatus");
     var statustext = $("#responsetext");
 
-    $("#restart-button").click(function(){
-        sendGpio(-1, true); // Stop notification
-
-        var xhr = new XMLHttpRequest();
-        if (DEBUG_MODE) {
-            xhr.open('GET', 'http://' + DEBUG_HTTP_SERVER + '/restart/');
-        } else {
-            xhr.open('GET', '/restart/?token=' + token);
-        }
-	xhr.send();
-    })
-
 
     $("#upload-button").click(function () {
         sendGpio(-1, true); // Stop notification
