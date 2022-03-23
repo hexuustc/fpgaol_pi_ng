d38ccb0a0aa7d6184761a9dc73623ae6f8cf5325
regymm <31496626+regymm@users.noreply.github.com>
2022-03-23 14:33:14 +0800

    frontend update

--- docroot/static/js/room.js
+++ docroot/static/js/room.js
@@ -16,12 +16,20 @@ var hw_init_json = JSON.stringify({
 	'id': -2,
 	'periphs': [{
 			'type': 'LED',
+			'idx' : 0,
+		}, {
+			'type': 'LED',
 			'idx' : 1,
+		}, {
+			'type': 'BTN',
+			'idx' : 0,
 		}, {
 			'type': 'BTN',
 			'idx' : 1,
 		}], 
 });
+
+var term_banner = '\x1B[1;1mFPGAOL \x1B[1;1;36mUART \x1B[1;1;32mxterm\x1B[1;1;33m.\x1B[1;1;31mjs \x1B[1;1;35m1.1\x1B[0m\r\n';
 
 function GetRequest() {
     var url = location.search;
@@ -68,26 +76,30 @@ $(document).ready(function () {
         console.log("receive message");
         var data = JSON.parse(e.data);
         var type = data['type'];
-        var values = data['values'];
-        if (type == 'LED') {
-            setLed(values);
-        } else if (type == 'SEG') {
-            setSeg(values);
-        } else if (type == 'WF') {
-            document.getElementById("download").innerHTML = "Download";
-            $("#download").attr("href", "./waveform.vcd");
-        } else if (type == 'MSG') {
-			term.write(values);
-			console.log("term output");
-        }
-        console.log("echo end");
+        var idx = data['idx'];
+		var payload = data['payload'];
+		if (type == 'LED') {
+			setLed(idx, payload);
+			var p = periphs[i];
+			console.log(p.type);
+		//} else if (type == 'BTN') {
+			//setSeg(values);
+		} else if (type == 'WF') {
+			document.getElementById("download").innerHTML = "Download";
+			$("#download").attr("href", "./waveform.vcd");
+		} else if (type == 'XDC') {
+			$("#copy-xdc").prop({'value': payload});
+		}
+			//term.write(values);
+			//console.log("term output");
+		//console.log("echo end");
     };
 
     // Waveform generation
     $("#generate").click(function () {
         $("#generate").attr("disabled", "true");
         document.getElementById("download").innerHTML = "Waiting...";
-        sendGpio(-1, false);
+		sendStopNotify();
     });
 
     notifySocket.onclose = function () {
@@ -101,6 +113,12 @@ $(document).ready(function () {
     $("#bitstream").change(function () {
         $("#file-name").val($("#bitstream").val());
     });
+	$("#submit-json").click(function () {
+		clear_periph();
+		prepare_periph();
+	});
+	$("#copy-xdc").click(function () {
+	});
 
     //for (var i = 0; i < 8; ++i) {
         //$("#sw" + i).change(function () {
@@ -132,7 +150,7 @@ $(document).ready(function () {
     var statustext = $("#responsetext");
 
     $("#upload-button").click(function () {
-        sendGpio(-1, true); // Stop notification
+        sendStopNotify();
 
         var file = $("#bitstream")[0].files[0];
         var zip = new JSZip();
@@ -168,8 +186,7 @@ $(document).ready(function () {
                         filestatus.removeClass("alert-danger");
                         filestatus.removeClass("alert-info");
                         filestatus.addClass("alert-success");
-						sendJson(hw_init_json);
-                        //sendGpio(-2, false);
+						sendStartNotify(hw_init_json);
                     } else {
                         $("#upload-button").removeAttr("disabled");
                         filestatus.removeClass("alert-success");
@@ -186,55 +203,111 @@ $(document).ready(function () {
             xhr.send(fd);
         });
     });
+
+	clear_everything();
+
+	//document.getElementById("jsoninput").value = hw_init_json;
+	$("#jsoninput").prop({'value': hw_init_json});
+	clear_periph();
+	prepare_periph();
+
 	// new xterm.js terminal
-	term = new Terminal();
-	term.open(document.getElementById('xterm'));
-	term.write('\x1B[1;1mFPGAOL \x1B[1;1;36mUART \x1B[1;1;32mxterm\x1B[1;1;33m.\x1B[1;1;31mjs \x1B[1;1;35m1.1\x1B[0m\r\n')
-	term.onData((val) => {
-		//term.write(val);
-		//sendGpio(-3, val);
-	})
-	// start data acquisition before web programming,
+	//term = new Terminal();
+	//term.open(document.getElementById('xterm'));
+	//term.write(term_banner);
+	//term.onData((val) => {
+		////term.write(val);
+		////sendGpio(-3, val);
+	//})
+	// start data acquisition after 1s, before web programming,
 	// because programming may has been done in Vivado XVC
 	setTimeout(function(){
-		sendGpio(-1, true);
-		clear_everything();
-		sendJson(hw_init_json);
+		sendStopNotify();
+		//clear_everything();
+		sendStartNotify(hw_init_json);
 	}, 1000);
 });
 
-function setLed(values) {
-    for (var i = 0; i < 8; ++i) {
-        $(".segplay:eq(" + i + ")").attr("value", Math.floor(values[i] % 10) ? "1" : "0");
-        $("#led" + i).prop({ 'checked': Math.floor(values[i] % 10) ? true : false });
-    }
+function setLED(idx, val) {
+	$("#led" + idx).prop({'checked': val ? true : false});
+}
+function SW_change(idx, val) {
+	console.log("SW", idx, val);
+	sendJson(JSON.stringify({
+		'type': 'BTN',
+		'idx': idx,
+		'payload': val
+	}));
 }
 
-function setSeg(values) {
-    for (var i = 0; i < 8; ++i) {
-        $("#hexplay_span" + i).html(values[i] == -1 ? '&nbsp;' : ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'b', 'C', 'd', 'E', 'F'][values[i]]);
-    }
-}
+//function setLed(values) {
+    //for (var i = 0; i < 8; ++i) {
+        //$(".segplay:eq(" + i + ")").attr("value", Math.floor(values[i] % 10) ? "1" : "0");
+        //$("#led" + i).prop({ 'checked': Math.floor(values[i] % 10) ? true : false });
+    //}
+//}
 
-function sendGpio(gpio, level) {
+//function setSeg(values) {
+    //for (var i = 0; i < 8; ++i) {
+        //$("#hexplay_span" + i).html(values[i] == -1 ? '&nbsp;' : ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'b', 'C', 'd', 'E', 'F'][values[i]]);
+    //}
+//}
+
+function sendStopNotify() {
     notifySocket.send(JSON.stringify({
-        'id': gpio,
-        'level': level,
+        'id': -1,
     }));
 }
 
+function sendStartNotify(json) {
+	var data = JSON.parse(json);
+	var id = data['id'];
+	if (id == -2) {
+		notifySocket.send(json);
+	}
+	else {
+		console.log("sendStartNotify: wrong id!");
+	}
+}
+
+// parse json on panel, initialize frontend peripherals
+function prepare_periph() {
+	var json = document.getElementById("jsoninput").value;
+	var data = JSON.parse(json);
+	var periphs = data['periphs'];
+	for(var i = 0; i < periphs.length; i++) {
+		var p = periphs[i];
+		console.log(p.type, p.idx);
+		if (p.type == 'LED') {
+			//
+		} else if (p.type == 'BTN') {
+			$('#sw' + p.idx).prop("disabled", false);
+			$('#sw' + p.idx).change(function() {
+				SW_change(this.id.slice(2), this.checked);
+			});
+		}
+	}
+
+}
+
+function clear_periph() {
+	for (var i = 0; i < 16; ++i) {
+		$("#sw" + i).prop({'checked': false, 'disabled': true});
+		$("#sw" + i).off('change');
+		//$("#sw" + i).hide();
+		$("#led" + i).prop({'checked': false});
+		$("#led" + i).off('change');
+		//$("#led" + i).addClass('d-none');
+	}
+}
+
 function sendJson(json) {
-    notifySocket.send(json);
+	console.log(json);
+	notifySocket.send(json);
 }
 
 function clear_everything() {
-    for (var i = 0; i < 8; ++i) {
-        $("#sw" + i).prop({ 'checked': false });
-    }
-
-    setSeg([-1, -1, -1, -1, -1, -1, -1, -1]);
-    setLed([0, 0, 0, 0, 0, 0, 0, 0]);
-
+	clear_periph();
     $("#upload-button").removeAttr("disabled");
     $("#generate").removeAttr("disabled");
     document.getElementById("download").innerHTML = "";
